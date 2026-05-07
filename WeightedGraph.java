ae7555177d506afbf19d453264fbf4adafd5af53
arunarauanovna07 <arunarauanovna07@gmail.com>
2026-05-07 17:15:20 +0500

    Add files via upload

---
+++ WeightedGraph.java
@@ -0,0 +1,16 @@
+package assignment4;
+import java.util.HashSet;
+import java.util.Set;
+
+public class WeightedGraph<V> {
+    private Set<Vertex<V>> vertices;
+    public WeightedGraph() {
+        vertices = new HashSet<>();
+    }
+    public void addVertex(Vertex<V> vertex) {
+        vertices.add(vertex);
+    }
+    public Set<Vertex<V>> getVertices() {
+        return vertices;
+    }
+}
